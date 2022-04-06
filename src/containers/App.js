8773e4fd4ca73845074ca67a19a3d3f6e83f1157
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-04-06 17:45:31 +0900

    📌::프로젝트 구조 설계

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Main } from "../pages/Pagelist";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Main/>}/>
+    </Routes>
+  );
+};
+
+export default App;
