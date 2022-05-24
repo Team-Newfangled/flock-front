8fd2d0697bbac838c28569733b60157988a70b87
seohyeon1578 <seohyeon1578@naver.com>
2022-05-24 20:16:31 +0900

    scorll issues fixed

--- src/pages/main/Second.js
+++ src/pages/main/Second.js
@@ -6,8 +6,8 @@ const Second = (props) => {
       <div className="second">
         <div className="mainBg">
           <img className="loginImg" src={require('../../images/login.svg').default} alt="로그인"/>
-          <img className={ props.scroll >= 10? 'mainText3 after-right': 'mainText3' } src={require('../../images/mainText3.svg').default} alt="OAUTH"/>
-          <img className={ props.scroll >= 10 ? 'mainText4 after-right': 'mainText4' } src={require('../../images/mainText4.svg').default} alt="간편하게 로그인 후 서비스를 이용하세요"/>
+          <img className={ props.scroll >= 10 && props.scroll <= 46 ? 'mainText3 after-right': 'mainText3' } src={require('../../images/mainText3.svg').default} alt="OAUTH"/>
+          <img className={ props.scroll >= 10 && props.scroll <= 46 ? 'mainText4 after-right': 'mainText4' } src={require('../../images/mainText4.svg').default} alt="간편하게 로그인 후 서비스를 이용하세요"/>
           <button className="startButton2"/>
         </div>  
       </div>
--- src/pages/main/Third.js
+++ src/pages/main/Third.js
@@ -5,8 +5,8 @@ const Third = (props) => {
     <>
       <div className="third">
         <div className="mainBg">
-          <img className={ props.scroll >= 40? 'mainText5 after-left': 'mainText5' } src={require('../../images/mainText5.svg').default} alt="체크리스트"/>
-          <img className={ props.scroll >= 40? 'mainText6 after-left': 'mainText6' } src={require('../../images/mainText6.svg').default} alt="더욱 간편하게 프로젝트를 수행하세요"/>
+          <img className={ props.scroll >= 40 && props.scroll <= 80? 'mainText5 after-left': 'mainText5' } src={require('../../images/mainText5.svg').default} alt="체크리스트"/>
+          <img className={ props.scroll >= 40 && props.scroll <= 80? 'mainText6 after-left': 'mainText6' } src={require('../../images/mainText6.svg').default} alt="더욱 간편하게 프로젝트를 수행하세요"/>
           <img className="chListIma" src={require('../../images/checkList.svg').default} alt="할 일"/>
         </div>  
       </div>
