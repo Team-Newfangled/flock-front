e18192fac617fbfc19306db9ac9cf4fae98fe8c6
seohyeon1578 <seohyeon1578@naver.com>
2022-05-26 20:31:21 +0900

    APIKEY & Login 스크롤 오류 고침

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import GoogleLogin from "react-google-login";
 import { ReactComponent as Logo } from '../../images/Logo.svg';
 import '../../styles/login.scss';
@@ -6,7 +6,9 @@ import '../../styles/login.scss';
 const Login = ({loginClick}) => {
 
   const REACT_APP_GOOGLE_API_KEY: API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
-  // const REACT_APP_GITHUB_API_KEY: API_KEY = process.env.REACT_APP_GITHUB_API_KEY;
+  const REACT_APP_GITHUB_API_KEY: API_KEY = process.env.REACT_APP_GITHUB_API_KEY;
+
+  const SCOPE = "openid";
 
   const onLoginSuccess = (response) => {
     console.log(response)
@@ -15,6 +17,11 @@ const Login = ({loginClick}) => {
   const onLoginFail = (response) => {
     console.log(response)
   }
+
+  const gitLogin = useCallback((): void => {
+    const url: string = `https://github.com/login/oauth/authorize?client_id=${REACT_APP_GITHUB_API_KEY}&scope=id,name,email,avatar_url`;
+    window.open(url);
+  }, [REACT_APP_GITHUB_API_KEY]);
 
   return (
     <>
@@ -37,11 +44,11 @@ const Login = ({loginClick}) => {
             onSuccess={onLoginSuccess}
             onFailure={onLoginFail}
             cookiePolicy={"single_host_origin"}
+            scope={SCOPE}
           />
-          <button className="githubLogin">
-            <img alt="oauthLogo" src={require('../../images/github.svg').default}/>
+          <button className="githubLogin" onClick={gitLogin}>
+            <img className="oauthLogo" alt="githubLogo" src={require('../../images/github.svg').default}/>
             <span>깃허브로 계속하기</span>
-            {/* <a href={REACT_APP_GITHUB_API_KEY}></a> */}
           </button>
           <span className="loginFnt">소셜계정을 통해 간편하게 로그인 하세요!</span>
         </div>
