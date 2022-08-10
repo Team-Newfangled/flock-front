d061c2147ab2383aa3a146dd6f8010cb8a826dd1
김민지 <wehouse201@naver.com>
2022-08-10 18:54:47 +0900

    calendar.css

--- src/pages/createTeam/CreateTeam.js
+++ src/pages/createTeam/CreateTeam.js
@@ -5,6 +5,7 @@ import '../../styles/CreateTeam.scss'
 import { NavLink } from "react-router-dom";
 import Calendar from 'react-calendar';
 import '../../styles/Calendar.scss';
+import moment from "moment";
 
 
 const CreateTeam = () => {
@@ -13,13 +14,20 @@ const CreateTeam = () => {
   return (
     <>
       <Header/>
+
+      <div>
+        
+      </div>
+
       <div className="dateBox">
         <NavLink to='/'><button id='link'>My Page</button></NavLink>
         <NavLink to='/'><button id='link'>My Team</button></NavLink>
         <div className="date">데드라인
           <Calendar
             onChange={onChange}
+            formatDay={(locale, date) => moment(date).format("DD")}
             value={value}
+            
           />
         </div>
       </div>
