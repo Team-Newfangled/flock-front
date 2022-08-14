5a99574f6312b7384a22ef183230e273bdd64db9
김민지 <wehouse201@naver.com>
2022-08-14 18:08:23 +0900

    team

--- src/pages/createTeam/teamcode/CodeUi.js
+++ src/pages/createTeam/teamcode/CodeUi.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../../styles/TeamCode.scss'
 
   const TeamCode = () =>{
 
@@ -7,11 +8,22 @@ import React from "react";
     <div className="h1Box">
           <h1>팀 정보</h1>
     </div>
-      <div className="profile">
-        <div className="profileBox">
+      <div className="team">
+        <div className="TeamBox">
           <h2 className="tName">팀이름</h2>
         </div>
-        <div></div>
+
+        <div className="people">
+          <h3>팀원관리</h3>
+        </div>
+
+        <div className="project">
+          <h3>팀원관리</h3>
+        </div>
+
+        <div className="accept">
+          <h3>팀원관리</h3>
+        </div>
       </div>
 
 
