c61795c863b63c8f4e9d80ee1e7f5bd18f25189a
seohyeon1578 <seohyeon1578@naver.com>
2022-08-22 20:35:45 +0900

    deadline marker

--- src/components/deadline/Mark/Mark.js
+++ src/components/deadline/Mark/Mark.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { ChromePicker } from "react-color";
+import todos from "./data.json";
 
 const Mark = () => {
-  
+  const [color, setColor] = useState('#8DF1CD');
+  const [isPicker, setIsPicker] = useState(true);
+
+  const pickerHandler = () => setIsPicker(!isPicker);
+
   return(
     <div className="mark">
-      <div className="deadlineList">
-        
+      <div className="deadline-list">
+        {todos.map((todo) => {
+        return(
+          <div className="deadline" key={todo.id}>
+            <div className="title">
+              <h2>{todo.name}</h2>
+              <div style={{ width: "20px",
+                            height: "20px",
+                            borderRadius: "50%",
+                            backgroundColor: todo.color}}>
+              </div>
+            </div>
+            <p>{todo["start-date"]} ~ {todo["end-date"]}</p>
+          </div>
+        )})}
       </div>
       <div className="change-deadline">
-
+          <h2>팀원 명</h2>
+          <div 
+              className="preview" 
+              onClick={pickerHandler} 
+              style={{backgroundColor: color}}>
+          </div>
+          <div className="picker" style={{display: isPicker ? '' : 'none'}}> 
+            <button className="close" onClick={pickerHandler}/>
+            <ChromePicker
+              color={color}
+              onChange={color => setColor(color.hex)}
+            />
+          </div>
+          <div className="change-date">
+            <h4>날짜</h4>
+          </div>
       </div>
     </div>
   );
