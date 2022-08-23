779aca9919abefdcdbf5602e3268a35bb867b450
김민지 <wehouse201@naver.com>
2022-08-23 17:50:32 +0900

    project

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -2,13 +2,23 @@ import TeamHeader from "../../components/header/Header";
 import '../../styles/Project.scss';
 import ToDo from "./ToDo.js";
 
+
 const Project = () => {
 
   return (
     <>
       <TeamHeader/>
       <ToDo/>
-
+      <div>
+        <div>
+          <img className='go' src={require('../../images/go.svg').default} />
+        </div>
+        <div className="iBox">
+          <img className='lineImg' src={require('../../images/lineBox.svg').default} />
+          <div className="space"></div>
+          <img className='feed' src={require('../../images/feed.svg').default} />
+        </div>
+      </div>
     </>
   )
 }
