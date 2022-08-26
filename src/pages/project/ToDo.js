9062b9b1f0df76a1fae101a4d48d028427c1e57a
김민지 <wehouse201@naver.com>
2022-08-26 15:40:11 +0900

    todo

--- src/pages/project/ToDo.js
+++ src/pages/project/ToDo.js
@@ -1,4 +1,4 @@
-import '../../styles/Project.scss';
+import '../../styles/Todo.scss';
 
 const ToDo = () => {
 
@@ -6,8 +6,11 @@ const ToDo = () => {
     <>
     <div className="todoBox">
       <h2>TO-DO!</h2>
-      <div className="list">
-
+      <div className="uplode">
+        <input className='upInput' type='text' placeholder="할 일을 추가해 주세요!"/>
+        <button className='upBtn' onClick={()=>{
+          
+        }}><img src={require('../../images/plus.svg').default} /></button>
       </div>
     </div>
     </>
