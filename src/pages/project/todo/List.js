77f27eff5478b502984595227ba7b164fa645adb
김민지 <wehouse201@naver.com>
2022-08-31 22:00:50 +0900

    css

--- src/pages/project/todo/List.js
+++ src/pages/project/todo/List.js
@@ -1,34 +1,11 @@
 import React from "react";
-import styled from 'styled-components';
 import Item from "./Item.js";
 import '../../../styles/Todo.scss'
- 
-const TodoList = styled.div`
-    position: relative;
-    top: 6%;
-    width: 90%;
-    height: 64%;
-    overflow: auto;
-    ::-webkit-scrollbar {
-      width: 14px;
-    }
-    ::-webkit-scrollbar-thumb {
-      background-color: #004DA780;
-      border-radius: 10px;
-      background-clip: padding-box;
-    }
-    ::-webkit-scrollbar-track {
-      background-color: #fff;
-      border-radius: 10px;
-    }
-`;
-
-
 
 function List() {
   return(
     <>
-    <TodoList>
+    <div className='TodoList'>
       <Item text="메롱" done={true}/>
       <Item text="커밋" done={false}/>
       <Item text="커밋" done={false}/>
@@ -39,7 +16,7 @@ function List() {
       <Item text="야!" done={true}/>
       <Item text="야!" done={true}/>
       <Item text="야!" done={true}/>
-    </TodoList>
+    </div>
     </>
   )
 }
--- src/pages/project/todo/Todo.js
+++ src/pages/project/todo/Todo.js
@@ -1,21 +1,8 @@
-import styled from 'styled-components';
-
-const TodoStyle = styled.div`
-  position: absolute;
-  top: 60%;
-  left: 16%;
-  transform: translateY(-60%);
-  width: 360px;
-  height: 660px;
-  padding: 20px;
-  border-radius: 30px;
-  background: #EFF2F9;
-  box-shadow: inset 0px 0px 10px rgba(0, 0, 0, 0.1);
-`
+import '../../../styles/Todo.scss'
 
 function Todo({children}) {
   return(
-    <TodoStyle>{children}</TodoStyle>
+    <div className='TodoStyle'>{children}</div>
   )
 }
 
