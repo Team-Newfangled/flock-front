89aaa49d58285b9a965004ed399be9a3732dbca1
leeyunseongKing <leeyunseong1004@gmail.com>
2022-09-02 11:56:06 +0900

    feed

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TeamHeader from "../../components/header/Header";
-import '../../styles/Project.scss';
+import '../../styles/Feed.scss';
 import Head from '../project/todo/Head.js';
 import List from '../project/todo/List.js';
 import Todo from "../project/todo/Todo.js";
@@ -8,10 +8,122 @@ const Feed = () => {
     return(
         <>
             <TeamHeader/>
-                <Todo>
+            <Todo>
                 <Head/>
                 <List/>
             </Todo>
+            <div className="feedmain">
+                <div className="feediteam">
+                    <div className="feedhead">
+                        <div>
+                            <img alt="user" src={require('../../images/userimg.svg').default}/>
+                            <p>팀원이름</p>
+                        </div>
+                        <img alt="user" src={require('../../images/detail.svg').default}/>
+                    </div>
+                    <div className="feedmean">
+                        <p className="commentmean"> 내용 </p>
+                        <div>
+                            <p>수정</p>
+                            <p>삭제</p>
+                        </div>
+                    </div>
+                    <div className="addadress">
+                        <img alt="user" src={require('../../images/addadress.svg').default}/>
+                    </div>
+                    <div className="feedaddress">
+                        <p>파일주소 www.dgsw.vk</p>
+                    </div>
+                    <br/>
+                    <div className="feedcommentwrite">
+                        <button>댓글쓰기</button>
+                    </div>
+                    <div className="feedcomments">
+                        <div>
+                            <div>
+                                <img alt="user" src={require('../../images/userimg.svg').default}/>
+                                <p>팀원이름</p>
+                            </div>
+                            <img alt="user" src={require('../../images/detail.svg').default}/>
+                        </div>
+                        <p className="commentmean">파일 확인좀....</p>
+                    </div>
+                </div>
+                
+                <div className="feediteam">
+                    <div className="feedhead">
+                        <div>
+                            <img alt="user" src={require('../../images/userimg.svg').default}/>
+                            <p>팀원이름</p>
+                        </div>
+                        <img alt="user" src={require('../../images/detail.svg').default}/>
+                    </div>
+                    <div className="feedmean">
+                        <p className="commentmean"> 내용 </p>
+                        <div>
+                            <p>수정</p>
+                            <p>삭제</p>
+                        </div>
+                    </div>
+                    <div className="addadress">
+                        <img alt="user" src={require('../../images/addadress.svg').default}/>
+                    </div>
+                    <div className="feedaddress">
+                        <p>파일주소 www.dgsw.vk</p>
+                    </div>
+                    <br/>
+                    <div className="feedcommentwrite">
+                        <button>댓글쓰기</button>
+                    </div>
+                    <div className="feedcomments">
+                        <div>
+                            <div>
+                                <img alt="user" src={require('../../images/userimg.svg').default}/>
+                                <p>팀원이름</p>
+                            </div>
+                            <img alt="user" src={require('../../images/detail.svg').default}/>
+                        </div>
+                        <p className="commentmean">파일 확인좀....</p>
+                    </div>
+                </div>
+                
+                <div className="feediteam">
+                    <div className="feedhead">
+                        <div>
+                            <img alt="user" src={require('../../images/userimg.svg').default}/>
+                            <p>팀원이름</p>
+                        </div>
+                        <img alt="user" src={require('../../images/detail.svg').default}/>
+                    </div>
+                    <div className="feedmean">
+                        <p className="commentmean"> 내용 </p>
+                        <div>
+                            <p>수정</p>
+                            <p>삭제</p>
+                        </div>
+                    </div>
+                    <div className="addadress">
+                        <img alt="user" src={require('../../images/addadress.svg').default}/>
+                    </div>
+                    <div className="feedaddress">
+                        <p>파일주소 www.dgsw.vk</p>
+                    </div>
+                    <br/>
+                    <div className="feedcommentwrite">
+                        <button>댓글쓰기</button>
+                    </div>
+                    <div className="feedcomments">
+                        <div>
+                            <div>
+                                <img alt="user" src={require('../../images/userimg.svg').default}/>
+                                <p>팀원이름</p>
+                            </div>
+                            <img alt="user" src={require('../../images/detail.svg').default}/>
+                        </div>
+                        <p className="commentmean">파일 확인좀....</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
   };
