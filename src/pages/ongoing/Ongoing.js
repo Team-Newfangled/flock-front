9c803593a9245847994f0afcfdd6d8d7bdb31abc
leeyunseongKing <leeyunseong1004@gmail.com>
2022-09-19 17:12:18 +0900

    issu

--- src/pages/ongoing/Ongoing.js
+++ src/pages/ongoing/Ongoing.js
@@ -7,39 +7,64 @@ import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
 
 const Ongoing = () => {
-    const [value, setValue] = useState(0);
-
+    const [value, setValue] = useState([0, 0])
+    const [members, setMembers] = useState(["이윤성", "김건호"])
     const handleClick = () => {
       setValue(0);
     };
-
+    const displayvalue = (idx, event) => {
+        console.log(event, idx)
+        console.log(value)
+        const temp = [...value]
+        temp[idx] = event[idx]
+        setValue(temp)
+    }
     return(
         <>
             <Header/>
             <Chat/>
+
             <div className="Ongoningiteam">
-                <div className="Ongoning myOngoning">
+                {/* <div className="Ongoning myOngoning">
                     <img alt="user" src={require('../../images/userimg.svg').default}/>
                     <div className="nouislider">
-                        <p>나</p>
-                        <div className="nouislider">
+                        <p className="nouislider-p">나</p>
+                        <div className="nouislider-1">
                             <Nouislider
-                                start={value}
-                                range={{
+                                start={value[0]}
+                                    range={{
                                     min: 0,
                                     max: 100
                                 }}
-                                connect
+                                connect onChange={displayvalue} 
                             />
+                            <p className="nouislider-2">진행도 : {value[0]}%</p>
                         </div>  
                     </div>
-                </div>
-                <div className="Ongoning">
-                    <img alt="user" src={require('../../images/userimg.svg').default}/>
-                    <div>
-                        <p>나</p>
+                </div> */}
+                {
+                    members.map((member,idx) => {
+                        return (
+                        <div className="Ongoning" key={idx}>
+                            <img alt="user" src={require('../../images/userimg.svg').default}/>
+                            <div className="nouislider">
+                        <p className="nouislider-p">나</p>
+                        <div className="nouislider-1">
+                            <Nouislider
+                                start={value[idx]}
+                                    range={{
+                                    min: 0,
+                                    max: 100
+                                }}
+                                connect onChange={(e) => displayvalue(idx, e)} 
+                            />
+                            <p className="nouislider-2">진행도 : {value[0]}%</p>
+                        </div>  
                     </div>
-                </div>
+                        </div>
+                        )
+                    })
+                }   
             </div>
         </>
     )}
