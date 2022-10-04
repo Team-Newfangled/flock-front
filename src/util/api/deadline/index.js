5da5202ed2fd6e2fefa8e8b66940f1f1b641e5dd
seohyeon1578 <seohyeon1578@naver.com>
2022-10-04 18:53:47 +0900

    deadline api 연결

--- src/util/api/deadline/index.js
+++ src/util/api/deadline/index.js
@@ -2,6 +2,8 @@ import { authAPI } from "../../../lib/API";
 
 export const getDeadline = async(projectId) => {
   await authAPI.get(`projects/${projectId}/deadline`)
+    .then((res) => { return res })
+    .catch((err) => { return err})
 };
 
 export const changeDeadlineColor = async({projectId, todoId, color}) => {
