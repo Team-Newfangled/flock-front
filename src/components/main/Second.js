9894f50831adcec632747d7299a0d62791a47b58
seohyeon1578 <seohyeon1578@naver.com>
2022-10-18 18:59:47 +0900

    deadline

--- src/components/main/Second.js
+++ src/components/main/Second.js
@@ -9,7 +9,7 @@ const Second = (props) => {
           <img className="loginImg" src={Login} alt="로그인"/>
           <img className={ props.scroll >= 10 && props.scroll <= 46 ? 'mainText3 after-right': 'mainText3' } src={require('../../images/mainText3.svg').default} alt="OAUTH"/>
           <img className={ props.scroll >= 10 && props.scroll <= 46 ? 'mainText4 after-right': 'mainText4' } src={require('../../images/mainText4.svg').default} alt="간편하게 로그인 후 서비스를 이용하세요"/>
-          <button className="startButton2"/>
+          <button className={ props.scroll >= 10 && props.scroll <= 46 ? "startButton2 after-right" : "startButton2" }/>
         </div>  
       </div>
     </>
--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,36 +1,16 @@
-import React,{ useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import qs from "qs";
+import React from "react";
 import Scroll from "../common/Scroll/scroll";
 import Layout from "./Layout";
 import First from "./First";
 import Second from "./Second";
 import Third from "./Third";
 import Fourth from "./Fourth";
-import { login } from "../../util/api/user";
-import { userlogin } from "../../store/users/userData";
 import "../../styles/main.scss";
 import "../../styles/css/main-slide.css";
 
 
 const Main = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   const scroll = Scroll();
-
-  useEffect(() => {
-    (async () => {
-      if(window.location.search.split('?').length > 1){
-        const { code } = qs.parse(window.location.search.split('?')[1])
-        const { data } = await login(code);
-        console.log(data)
-        dispatch(userlogin(data));
-         
-        navigate('/');      // re-rendering 방지
-      }
-    })();
-  }, [])
 
   return(
     <Layout>
