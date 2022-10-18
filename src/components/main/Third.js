5a696530141c772b4ef7a4b517d0b63ef083a8ed
김민지 <wehouse201@naver.com>
2022-10-18 18:16:37 +0900

    png

--- src/components/main/Third.js
+++ src/components/main/Third.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ChekList from '../../images/checkList.png'
 
 const Third = (props) => {
   return (
@@ -7,7 +8,7 @@ const Third = (props) => {
         <div className="mainBg">
           <img className={ props.scroll >= 40 && props.scroll <= 80? 'mainText5 after-left': 'mainText5' } src={require('../../images/mainText5.svg').default} alt="체크리스트"/>
           <img className={ props.scroll >= 40 && props.scroll <= 80? 'mainText6 after-left': 'mainText6' } src={require('../../images/mainText6.svg').default} alt="더욱 간편하게 프로젝트를 수행하세요"/>
-          <img className="chListIma" src={require('../../images/checkList.svg').default} alt="할 일"/>
+          <img className="chListIma" src={ChekList} alt="할 일"/>
         </div>  
       </div>
     </>
